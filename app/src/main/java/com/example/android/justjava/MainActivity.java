35f78db5dbf97f1450f7d6e601c510bd110e95aa
Veselin Cholakov <Veselin.Cholakov@bulpros.com>
2017-01-20 17:01:03 +0200

    First

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.justjava;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int number;
+    private int price= 2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(5);
+    }
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        this.number += number;
+        quantityTextView.setText("" + this.number);
+        displayPrice(this.number*price);
+    }
+}
